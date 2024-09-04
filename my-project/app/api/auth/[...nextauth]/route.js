4659d64e8c4c7b9db14e7f819aa05bc5a2c33e40
yogasuba <yogeshwarianbarasan@gmailcom>
2024-09-04 10:48:57 +0530

    Update route.js

--- my-project/app/api/auth/[...nextauth]/route.js
+++ my-project/app/api/auth/[...nextauth]/route.js
@@ -1,17 +1,16 @@
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 
-
-const handler = NextAuth({
+export default NextAuth({
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
-    // Add other providers here if needed
   ],
-
-
+  callbacks: {
+    async redirect({ url, baseUrl }) {
+      return baseUrl;
+    },
+  },
 });
-
-export { handler as GET, handler as POST };
